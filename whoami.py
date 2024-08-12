f2786342279b160f419976a5004cd5808909c018
whoami <j3yablo4koo@gmail.com>
2024-08-12 05:56:18 +0200

    Add files via upload

--- whoami.py
+++ whoami.py
@@ -5,6 +5,7 @@ from random import choice
 from colorama import Fore
 import fade
 from tqdm import tqdm
+import subprocess
 
 # Добавляем путь к родительскому каталогу, чтобы импортировать модули из Core
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -83,6 +84,22 @@ def main():
         if number:
             main()
 
+    def check_for_updates():
+        # Получение текущей ветки
+        current_branch = subprocess.getoutput("git rev-parse --abbrev-ref HEAD")
+
+        # Сравнение локального и удаленного репозиториев
+        subprocess.run(["git", "fetch", "origin", current_branch])
+
+        local_commit = subprocess.getoutput("git rev-parse HEAD")
+        remote_commit = subprocess.getoutput(f"git rev-parse origin/{current_branch}")
+
+        return local_commit != remote_commit
+
+    def update_application():
+        # Выполнение команды для обновления локального репозитория
+        subprocess.run(["git", "pull"])
+
     def type_attack_change():
         '''Изменение типа атаки'''
         new_type_attack = input(Fore.RED + "Введите тип атаки (MIX, SMS, CALL): ").strip().upper()
@@ -128,7 +145,7 @@ def main():
         clear()
         print(fade.purplepink(_banner))
         print(fade.fire('если я узнаю что этот бомбер куда-то сливался, то я \nсделаю запуск по токенам и хуй вам черти ебаные'))
-        print(fade.fire("\n1. Запуск атаки\n2. Изменить тип атаки\n3. Включить/выключить сервисы обратной связи\n4. Инструкция\n5. Выход"))
+        print(fade.fire("\n1. Запуск атаки\n2. Изменить тип атаки\n3. Включить/выключить сервисы обратной связи\n4. Инструкция\n5. Обновить (автоматически)\n6. Выход"))
         choice = input(Fore.RED + "Выберите опцию: ").strip()
 
         if choice == '1':
@@ -140,6 +157,22 @@ def main():
         elif choice == '4':
             instruction()
         elif choice == '5':
+            if check_for_updates():
+                clear = lambda: os.system('cls' if os.name=='nt' else 'clear')
+                clear()
+                print(Fore.GREEN + "Найдены обновления. Обновление приложения...")
+                update_application()
+                print(Fore.GREEN + "Приложение обновлено. Перезапустите его.")
+                number = input(Fore.RED + "Вернуться - Enter").strip()
+                if number:
+                    main()
+                break
+            else:
+                print(Fore.RED + "Обновлений нет.")
+                number = input(Fore.RED + "Вернуться - Enter").strip()
+                if number:
+                    main()
+        elif choice == '6':
             break
         else:
             print(Fore.RED + "Неверный выбор. Попробуйте снова.")
