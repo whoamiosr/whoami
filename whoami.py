5eae1dd129c5c702a542e7f0ec3455d44b9629d8
whoami <j3yablo4koo@gmail.com>
2024-08-12 04:37:06 +0200

    Add files via upload

---
+++ whoami.py
@@ -0,0 +1,148 @@
+import sys
+import os
+from time import sleep
+from random import choice
+from colorama import Fore
+import fade
+from tqdm import tqdm
+
+# Добавляем путь к родительскому каталогу, чтобы импортировать модули из Core
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from Core.Config import *
+from Core.Run import start_async_attacks
+from Core.Attack.Services import urls
+from Core.Attack.Feedback_Services import feedback_urls
+
+def main():
+    '''Консольный интерфейс бомбера'''
+
+
+    _banner = '''
+██╗    ██╗██╗  ██╗ ██████╗  █████╗ ███╗   ███╗██╗
+██║    ██║██║  ██║██╔═══██╗██╔══██╗████╗ ████║██║
+██║ █╗ ██║███████║██║   ██║███████║██╔████╔██║██║
+██║███╗██║██╔══██║██║   ██║██╔══██║██║╚██╔╝██║██║
+╚███╔███╔╝██║  ██║╚██████╔╝██║  ██║██║ ╚═╝ ██║██║
+ ╚══╝╚══╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═╝     ╚═╝╚═╝???
+                                                 
+ v.1.0.1 beta
+'''
+
+    _dockBanner = '''⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⠀⠀⠀⠀⡀
+⠀⠀⠀⢲⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⣦⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⣶⠀⠀⠀⠀⢸⠁
+⠀⠀⠀⣿⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⡇⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⣿⠀⠀⠀⠀⢸⡆
+⠀⠀⠀⣿⡀⢀⠀⠀⣸⣇⢀⠀⠤⢠⣿⠄⡄⢀⡠⣼⡤⣀⠀⡄⢸⣧⠠⡀⢠⢤⣿⡤⡄⠠⠔⣾⡗⠢
+⠀⠀⠀⣿⠁⠀⠀⠀⢻⡏⠀⠀⠀⢠⣿⠀⠀⠀⠀⣿⡇⠀⠀⠀⢸⣿⠀⠀⠀⠘⣿⠃⠀⠀⠀⣿⡇
+⠀⠀⠀⣿⠀⠀⠀⠀⢸⡇⠀⠀⠀⠘⣿⠀⠀⠀⠀⣿⡇⠀⠀⠀⢸⡿⠀⠀⠀⢀⣿⡀⠀⠀⠀⣿⡇
+⠀⠀⠀⣿⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⣿⠀⠀⠀⠀⣿⡇⠀⠀⠀⢸⡇⠀⠀⠀⠈⣿⠁⠀⠀⠀⣿⡇
+⠀⠀⠀⣿⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⣿⠀⠀⠀⠀⢸⠁⠀⠀⠀⢸⡇⠀⠀⠀⠀⣿⠀⠀⠀⠀⣿⡇
+⠀⠀⠀⣿⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⣿⠀⠀⠀⠀⢸⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⣿⠀⠀⠀⠀⣿⡇
+⠀⠀⠀⣿⠀⠀⠀⠀⢸⡇⠀⠀⠀⢸⣿⠀⠀⠀⠀⢸⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⣿⠀⠀⠀⠀⣿⡇
+⠀⠀⠀⣿⠀⠀⠀⠀⢸⡇⠀⠀⠀⠸⣿⠀⠀⠀⠀⢸⠀⠀⠀⠀⢸⡇⠀⠀⠀⠀⣿⠀⠀⠀⠀⢻⡇
+⠀⠀⠀⠈⠀⠀⠀⠀⠘⠃⠀⠀⠀⠀⠋⠀⠀⠀⠀⠸⠀⠀⠀⠀⠀⠇⠀⠀⠀⠀⠉⠀⠀⠀⠀⠘⠁
+          by whoami дэнчик йоу
+   ⣶⣶⣶⣶⣾⡷⢿⣶⣶⡾⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⣾⣿⣿⣿⣷⣶⣶⣶⣶⣶⣶⣾⣿⣿'''
+
+
+
+    # Инициализация конфигурации
+    config = check_config()
+
+    def instruction():
+        clear = lambda: os.system('cls' if os.name=='nt' else 'clear')
+        clear()
+        print(fade.fire('''
+1. Упала интенсивность атаки.
+ В случае, если интенсивность атаки значительно упала - прекратите атаку и подождите некоторое время.
+ Так происходит из-за того, что сервисы добавляют ваш ip в чёрный список из-за слишком большого кол-ва запросов.
+                
+2. Атака не началась.
+ Если жертве не приходят SMS сообщения, попробуйте переустановить репозиторий и обновить библиотеки.
+ Также убедитесь что номер начинается на +7 или +998 (Россия и Узбекистан соответственно)
+                
+3. Утилита неисправна или работает неккоректно.
+ Переустановите или ждите, пока дядя разработчик всё пофиксит.
+'''))
+        print(fade.greenblue('''
+░░░░░░░░░░░░░░░▄▄░░░░░░░░░░░
+░░░░░░░░░░░░░░█░░█░░░░░░░░░░
+░░░░░░░░░░░░░░█░░█░░░░░░░░░░
+░░░░░░░░░░░░░░█░░█░░░░░░░░░░
+░░░░░░░░░░░░░░█░░█░░░░░░░░░░
+██████▄███▄████░░███▄░░░░░░░
+▓▓▓▓▓▓█░░░█░░░█░░█░░░███░░░░
+▓▓▓▓▓▓█░░░█░░░█░░█░░░█░░█░░░
+▓▓▓▓▓▓█░░░░░░░░░░░░░░█░░█░░░
+▓▓▓▓▓▓█░░░░░░░░░░░░░░░░█░░░░
+▓▓▓▓▓▓█░░░░░░░░░░░░░░██░░░░░
+▓▓▓▓▓▓█████░░░░░░░░░██░░░░░
+█████▀░░░░▀▀████████░░░░░░
+'''))
+        number = input(Fore.RED + "Вернуться - Enter").strip()
+        if number:
+            main()
+
+    def type_attack_change():
+        '''Изменение типа атаки'''
+        new_type_attack = input(Fore.RED + "Введите тип атаки (MIX, SMS, CALL): ").strip().upper()
+        if new_type_attack in ['MIX', 'SMS', 'CALL']:
+            change_config('type_attack', new_type_attack)
+        else:
+            print(Fore.RED + "Неверный тип атаки!")
+
+    def feedback_change():
+        '''Включение/выключение сервисов обратной связи'''
+        feedback_status = input(Fore.RED + "Включить сервисы обратной связи? (yes/no): ").strip().lower()
+        change_config('feedback', 'True' if feedback_status == 'yes' else 'False')
+
+    def start_attack(number):
+        '''Запуск атаки'''
+        print(Fore.GREEN + "Атака запущена! (надеюсь)" + Fore.RED +"\nВ случае, если атака не запустилась - читайте инструкцию.")
+        
+        change_config('attack', 'True')
+        try:
+            while True:
+                start_async_attacks(number, 1)
+        except KeyboardInterrupt:
+            print(Fore.RED + "Атака остановлена пользователем.")
+        finally:
+            change_config('attack', 'False')
+            print(Fore.RED + "Атака завершена.")
+
+    def checking_values():
+        clear = lambda: os.system('cls' if os.name=='nt' else 'clear')
+        clear()
+        '''Проверка вводимых данных'''
+        print(fade.fire(_dockBanner))
+        number = input(Fore.RED + "Введите атакующий номер (без + и пробелов): ").strip()
+        if number.isdigit():
+            start_attack(number)
+        else:
+            print(Fore.RED + "Введите корректный номер!")
+
+
+
+    while True:
+        clear = lambda: os.system('cls' if os.name=='nt' else 'clear')
+        clear()
+        print(fade.purplepink(_banner))
+        print(fade.fire('если я узнаю что этот бомбер куда-то сливался, то я \nсделаю запуск по токенам и хуй вам черти ебаные'))
+        print(fade.fire("\n1. Запуск атаки\n2. Изменить тип атаки\n3. Включить/выключить сервисы обратной связи\n4. Инструкция\n5. Выход"))
+        choice = input(Fore.RED + "Выберите опцию: ").strip()
+
+        if choice == '1':
+            checking_values()
+        elif choice == '2':
+            type_attack_change()
+        elif choice == '3':
+            feedback_change()
+        elif choice == '4':
+            instruction()
+        elif choice == '5':
+            break
+        else:
+            print(Fore.RED + "Неверный выбор. Попробуйте снова.")
+
+if __name__ == "__main__":
+    main()
